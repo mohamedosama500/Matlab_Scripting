9892a3448fd43f2e1f74847c2aab4704a28509b8
mohamedosama500 <muhammed.osama012@gmail.com>
2025-03-04 23:45:35 +0200

    Updating_01 the Matlab_scripting Tutorial

--- Matlab_Scripting.m
+++ Matlab_Scripting.m
@@ -1,3 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Concatenation Methods
+
+A = magic(3)
+B = ones (3,2)
+C = [A, B]
+D = [A; B']
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Preallocation
+
+tic
+%instead of using x = 0; , it will take more time
+x= zeros(1, 1000000);
+for k = 2: 1000000
+    x(k) = x(k-1) +5;
+end
+
+toc
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Access data in a cell array
 c = { 'one', 'two' 'three';1,2,3};
 Data = c(1,2)
 class(Data)
